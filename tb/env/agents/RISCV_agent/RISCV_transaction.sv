6b9daccf6383de45d27143c5651e9d797899dbc0
Davidmachadocb <davidmachadocbec@gmail.com>
2025-07-04 09:38:41 -0300

    Random logic indicated.

--- tb/env/agents/RISCV_agent/RISCV_transaction.sv
+++ tb/env/agents/RISCV_agent/RISCV_transaction.sv
@@ -23,7 +23,7 @@ class RISCV_transaction extends uvm_sequence_item;
   // Instruction type name, useful for debugging and logging
   string instr_name;
 
-  // 🆕 Additional signals used by scoreboard and environment
+
   rand bit [31:0] data_mem_address;
   rand bit [31:0] data_mem_write_data;
   rand bit        data_mem_write_enable;
@@ -48,7 +48,7 @@ class RISCV_transaction extends uvm_sequence_item;
     `uvm_field_int(data_wr_en_ma,             UVM_ALL_ON)
     `uvm_field_string(instr_name,             UVM_ALL_ON)
 
-    // 🆕 New fields for scoreboard/env
+    
     `uvm_field_int(data_mem_address,          UVM_ALL_ON)
     `uvm_field_int(data_mem_write_data,       UVM_ALL_ON)
     `uvm_field_int(data_mem_write_enable,     UVM_ALL_ON)
@@ -68,7 +68,7 @@ class RISCV_transaction extends uvm_sequence_item;
    * Method: post_randomize
    */
   function void post_randomize();
-    // Optional: colocar lógica de pós-randomização aqui, se necessário
+    // colocar lógica de pós-randomização aqui!!!!!
   endfunction 
 
 endclass
