30c0ebd337d36ca3e32dff0e18c7b4ca03432b46
Glenda Barbosa <136124553+glenda1235@users.noreply.github.com>
2025-07-07 15:13:22 -0300

    Update RISCV_defines.sv

--- tb/env/agents/RISCV_agent/RISCV_defines.sv
+++ tb/env/agents/RISCV_agent/RISCV_defines.sv
@@ -1,11 +1,4 @@
-//------------------------------------------------------------------------------
-// Definitions and macros for RISCV agent
-//------------------------------------------------------------------------------
-// This file contains definitions and macros used by the RISCV agent.
-//
-// Author: Gustavo Santiago
-// Date  : June 2025
-//------------------------------------------------------------------------------
+
 
 `ifndef RISCV_DEFINES
 `define RISCV_DEFINES
